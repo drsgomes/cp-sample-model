a26d6d3c86f4d139b8d729a7fe607f660d58185a
Danilo Gomes <dgomes@accela.com>
2018-03-07 14:57:33 +0000

    Initial Commit

---
+++ cp-sample-model/cp_sample_model.h
@@ -0,0 +1,19 @@
+//
+//  cp_sample_model.h
+//  cp-sample-model
+//
+//  Created by Danilo Gomes on 3/7/18.
+//  Copyright © 2018 Accela Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cp_sample_model.
+FOUNDATION_EXPORT double cp_sample_modelVersionNumber;
+
+//! Project version string for cp_sample_model.
+FOUNDATION_EXPORT const unsigned char cp_sample_modelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cp_sample_model/PublicHeader.h>
+
+
